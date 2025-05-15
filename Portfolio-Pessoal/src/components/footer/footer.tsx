4ac45f923337b89a9951d4f105a8c4ec6dc72bdf
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2025-05-15 12:47:38 -0300

    Portfolio completo, falta apenas adicionar os projetos
    .

--- Portfolio-Pessoal/src/components/footer/footer.tsx
+++ Portfolio-Pessoal/src/components/footer/footer.tsx
@@ -8,7 +8,26 @@ export const Footer = () =>{
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="LinkedIn" width="32" height="32" />
                 </a>
                 <a href="#sobre" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" width="32" height="32" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                    <rect width="100%" height="100%" fill="white" rx="8"/>
+                    <path fill="black" d="M12 .296c-6.63 0-12 5.373-12 12 0 5.303 
+                        3.438 9.8 8.205 11.387.6.113.82-.258.82-.577 
+                        0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.084-.729.084-.729 
+                        1.205.084 1.84 1.236 1.84 1.236 1.07 1.835 2.809 
+                        1.304 3.495.997.108-.776.418-1.305.76-1.605-2.665-.3-5.466-1.334-5.466-5.93 
+                        0-1.31.47-2.38 1.236-3.22-.124-.303-.536-1.523.117-3.176 
+                        0 0 1.008-.322 3.3 1.23.957-.266 1.98-.399 
+                        3-.405 1.02.006 2.043.139 3 
+                        .405 2.29-1.552 3.297-1.23 
+                        3.297-1.23.653 1.653.242 2.873.118 
+                        3.176.77.84 1.234 1.91 1.234 3.22 
+                        0 4.61-2.803 5.625-5.475 
+                        5.92.43.37.823 1.102.823 2.222 
+                        0 1.606-.015 2.898-.015 
+                        3.293 0 .32.218.694.825.576C20.565 
+                        22.092 24 17.592 24 12.296c0-6.627-5.373-12-12-12z"/>
+                    </svg>
+
                 </a>
                 <a href="#sobre" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" width="32" height="32" />
@@ -17,7 +36,7 @@ export const Footer = () =>{
 
                 {/* Copyright */}
                 <p className="text-sm text-white">
-                &copy; {new Date().getFullYear()} Victor Vaz. Todos os direitos reservados.
+                &copy; {new Date().getFullYear()} Victor Vaz. Obrigado pelo seu tempo!
                 </p>
             </div>
         </div>
--- Portfolio-Pessoal/src/components/skills/skills.tsx
+++ Portfolio-Pessoal/src/components/skills/skills.tsx
@@ -1,13 +1,25 @@
+import RotatingText from "../rotatingText/rotatingText"
 import { Marquee } from "../magicui/marquee"
-
 
 export const Skills = () =>{
     return(
         <div className="flex flex-col justify-center items-center mt-[6vh] mb-[10vh]" id="skills">
-            <h1 className="text-[3.5rem] text-white font-semibold  mb-[6vh]">Habilidades</h1>
+            <h1 className="text-[3.5rem] text-white font-semibold  mb-[6vh]">Habilidades <RotatingText
+                    texts={['Front-end', 'Dados', 'Backend']}
+                    mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                    staggerFrom={"last"}
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    exit={{ y: "-120%" }}
+                    staggerDuration={0.025}
+                    splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                    transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                    rotationInterval={2000}
+                    /></h1>
+
             <div className="flex justify-center items-center">
-                <div className="gap-10 text-white">
-                    <Marquee pauseOnHover>
+                <div className="mt-[4vh]">
+                   <Marquee pauseOnHover>
                         <div className="relative w-[400px] p-4 rounded-lg border-2 border-gray-950 bg-gray-950 text-white font-semibold flex justify-center items-center">
                             <p>React.js (Vite, Native e Next)</p>
                         </div>
