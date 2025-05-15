dfe28ef6c05684f7531628b74ffb593a5c129fb1
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2025-05-15 04:09:19 -0300

    atualização footer
    .

--- Portfolio-Pessoal/src/components/footer/footer.tsx
+++ Portfolio-Pessoal/src/components/footer/footer.tsx
@@ -1,22 +1,22 @@
 export const Footer = () =>{
     return(
-        <div className="w-full bg-[#33cccc] text-white shadow-md py-6 mt-[4vh]" id="contatos">
+        <div className="w-full bg-[#0f0f0f] text-white shadow-md py-6 mt-[4vh]" id="contatos">
             <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-4">
                 {/* Redes Sociais */}
                 <div className="flex space-x-4">
-                <a href="#sobre" target="_blank" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
+                <a href="#sobre" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="LinkedIn" width="32" height="32" />
                 </a>
-                <a href="#sobre" target="_blank" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
+                <a href="#sobre" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" width="32" height="32" />
                 </a>
-                <a href="#sobre" target="_blank" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
+                <a href="#sobre" rel="noopener noreferrer" className="hover:text-[#33cccc] transition">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" width="32" height="32" />
                 </a>
                 </div>
 
                 {/* Copyright */}
-                <p className="text-sm text-black">
+                <p className="text-sm text-white">
                 &copy; {new Date().getFullYear()} Victor Vaz. Todos os direitos reservados.
                 </p>
             </div>
