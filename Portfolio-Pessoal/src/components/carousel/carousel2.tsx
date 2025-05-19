06d8692c967d8b74ac88e58ccd77f3804994bad0
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2025-05-19 00:53:27 -0300

    subindo comentario
    .

--- Portfolio-Pessoal/src/components/carousel/carousel2.tsx
+++ Portfolio-Pessoal/src/components/carousel/carousel2.tsx
@@ -40,7 +40,7 @@ const DEFAULT_ITEMS: CarouselItem[] = [
     {
         title: "Plataforma de Cadastro",
         description:
-            "Plataforma web desenvolvida com HTML, CSS e JavaScript, usando Vite. Possui interface responsiva e interativa, com busca de endereço via API do ViaCEP. Os dados dos clientes são salvos em tempo real no Firebase, ideal para aprendizado de aplicações full stack.",
+            "Plataforma web desenvolvida com HTML, CSS e JavaScript, usando Vite. Possui interface responsiva e interativa, com busca de endereço via API do ViaCEP. Os dados dos clientes são salvos em tempo real no Firebase, ideal para aprendizado de aplicações full stack.<br/> Tecnologias usadas: Vite, Firebase e APi ViaCEP",
         id: 1,
         icon: <FiShoppingCart className="h-[16px] w-[16px] text-white" />,
         imageUrl: LoginESG,
